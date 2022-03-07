25db69ca848cf953e5ce4d15949e1df984b21962
向皓田 <leoshiang@gmail.com>
2022-03-07 17:44:37 +0800

    0.0.5 修正匯出物件。

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const { DocumentCollection } = require('./src/DocumentCollection')
-const { Document } = require('./src/Document')
 const { FoodServicesDataset } = require('./src/FoodServicesDataset')
+const { Document } = require('./src/Document')
+const { DocumentCollection } = require('./src/DocumentCollection')
 const { Tokenizer } = require('./src/Tokenizer')
-const { TokenizationResult } = require('./src/TokenizationResult')
 const { TFIDF } = require('./src/TFIDF')
-const { Jieba } = require('./src/Jieba')
+const { NLP } = require('./src/NLP')
 
 module.exports = {
-  Corpus: DocumentCollection,
-  Document,
   FoodServicesDataset,
+  Document,
+  DocumentCollection,
   Tokenizer,
-  Tokens: TokenizationResult,
   TFIDF,
-  Jieba
+  NLP,
 }
